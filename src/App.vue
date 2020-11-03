1cd29c52520fcecf925707dd94d0534e7e00be05
liang.cheng <chengliang.chrome@gmail.com>
2020-11-03 21:52:20 +0800

    开工大吉

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-container>
+    <el-header class="header">Header</el-header>
+    <el-container class="content">
+      <el-aside width="200px" class="aside">
+        <LHMenu/>
+      </el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+import LHMenu from './components/LHMenu'
+export default {
+  components: {
+    LHMenu,
+  }
+}
+</script>
+
+<style lang="scss">
+.header {
+  height: 15vh;
+}
+.content {
+  .aside {
+    width: 20vw;
+  }
+  .main {
+    flex: 1 1 auto;
+  }
+}
+</style>
---
+++ src/components/LHMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <scroll-bar>
+    <el-menu class="el-menu-vertical-demo">
+      <el-submenu index="1">
+        <template slot="title">
+          <span>数据库</span>
+        </template>
+        <el-menu-item-group>
+          <template slot="title"><div></div></template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </scroll-bar>
+</template>
+
+<script>
+export default {
+  name: 'lh-menu',
+}
+</script>
+<style>
+</style>
